f0b2d2d4adb26ca86846ecd137abe4b4f3819642
Kogito Tooling Bot (kiegroup) <kogito-tooling-bot@gmail.com>
2020-07-06 19:04:56 +0000

    0.6.0 resources

---
+++ editors/0.6.0/bpmn/org.kie.workbench.common.stunner.kogito.KogitoBPMNEditor/deferredjs/2E56ABC9C9BE70E6B1B2B1D54523164B/1.cache.js
@@ -0,0 +1,1 @@
+$wnd.org_kie_workbench_common_stunner_kogito_KogitoBPMNEditor.runAsyncCallback1("var M8c=wni(CMD,'RunAsyncCallback');ENh(16490,1,{16797:1});_.kq=function xcm(){null.iib()};var O0d=uni(vvE,'AsyncBeanFactory/1',16490,pmd);DLD(mrc)(1);\n//# sourceURL=org.kie.workbench.common.stunner.kogito.KogitoBPMNEditor-1.js\n")
