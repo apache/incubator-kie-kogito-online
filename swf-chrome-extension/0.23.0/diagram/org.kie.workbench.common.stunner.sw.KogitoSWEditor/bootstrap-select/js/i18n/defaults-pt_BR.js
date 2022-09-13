9e6eadc553cad07d1da70e5685ebbe77774b66c6
KIE Tools Bot (kiegroup) <kietoolsbot@gmail.com>
2022-09-13 15:12:02 +0000

    Deploy 0.23.0 (Chrome Extension for Serverless Workflow Editor)

---
+++ swf-chrome-extension/0.23.0/diagram/org.kie.workbench.common.stunner.sw.KogitoSWEditor/bootstrap-select/js/i18n/defaults-pt_BR.js
@@ -0,0 +1,2 @@
+/*! For license information please see defaults-pt_BR.js.LICENSE.txt */
+!function(e,t){"function"==typeof define&&define.amd?define(["jquery"],(function(e){return t(e)})):"object"==typeof exports?module.exports=t(require("jquery")):t(jQuery)}(0,(function(e){e.fn.selectpicker.defaults={noneSelectedText:"Nada selecionado",noneResultsText:"Nada encontrado contendo {0}",countSelectedText:"Selecionado {0} de {1}",maxOptionsText:["Limite excedido (máx. {n} {var})","Limite do grupo excedido (máx. {n} {var})",["itens","item"]],multipleSeparator:", "}}));
