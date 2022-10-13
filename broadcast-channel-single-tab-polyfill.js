5e759e57d05eabfd80ca7eddabf2540fc296d3f0
KIE Tools Bot (kiegroup) <kietoolsbot@gmail.com>
2022-10-13 13:30:58 +0000

    Deploy 0.24.0 (Editors + Online Editor)

--- broadcast-channel-single-tab-polyfill.js
+++
@@ -1,1 +0,0 @@
-!function(){if("BroadcastChannel"in window)return;const e=new Map;window.BroadcastChannel=class{constructor(t){this.name=t,e.get(t)||e.set(t,new Set),e.get(t).add(this)}postMessage(t){setTimeout((()=>{e.get(this.name).forEach((e=>{var s;e!==this&&(null===(s=e.onmessage)||void 0===s||s.call(e,{data:t}))}))}),0)}close(){e.get(this.name).delete(this)}}}();
