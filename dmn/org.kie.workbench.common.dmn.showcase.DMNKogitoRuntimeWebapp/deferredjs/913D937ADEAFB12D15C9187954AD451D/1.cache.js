7415590c404c17b0cfed758b8bc40d6f789074d3
Kennedy Bowers <kennedy.bowers@ibm.com>
2023-11-15 12:22:52 +0100

    incubator-kie-issues#573:replace copyrights headers/comments in kogito-online

--- dmn/org.kie.workbench.common.dmn.showcase.DMNKogitoRuntimeWebapp/deferredjs/913D937ADEAFB12D15C9187954AD451D/1.cache.js
+++ dmn/org.kie.workbench.common.dmn.showcase.DMNKogitoRuntimeWebapp/deferredjs/913D937ADEAFB12D15C9187954AD451D/1.cache.js
@@ -1,1 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 $wnd.org_kie_workbench_common_dmn_showcase_DMNKogitoRuntimeWebapp.runAsyncCallback1("function Ufd(){Ufd=a6i}\nvar epe=xgk('com.google.gwt.core.client','RunAsyncCallback');function rDn(){rDn=a6i;zb()}\nfunction tDn(a,b){rDn();this.d=a;this.c=b;Gb.call(this);this.pWc()}\n$5i(14733,1,{3298:1,1:1},tDn);_.pWc=function sDn(){};_.dnb=function uDn(){this.d.Mmc(this.c)};c4O(Qgd)(1);\n//# sourceURL=org.kie.workbench.common.dmn.showcase.DMNKogitoRuntimeWebapp-1.js\n")
